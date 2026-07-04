d408fbe6bafce07b3d4dd0dc501f085e20c48238
Mohammad Polaei <mohammadpolaei@gmail.com>
2026-07-04 16:08:22 +0330

    feat : currency section created

--- src/features/home-page/hero-section/hero-section.tsx
+++ src/features/home-page/hero-section/hero-section.tsx
@@ -1,6 +1,40 @@
+import adaIcon from "@/assets/hero-section/ADA.svg";
+import ethIcon from "@/assets/hero-section/ETH.svg";
+import solIcon from "@/assets/hero-section/SOL.svg";
+
+import acsIcon from "@/assets/hero-section/acsIcon.svg";
+import decIcon from "@/assets/hero-section/decIcon.svg";
+
+const currencyList = [
+	{
+		icon: ethIcon.src,
+		title: "Ethereum",
+		symbol: "ETH",
+		price: "3,245.67",
+		change: "1.8",
+		changeStat: "acs",
+	},
+	{
+		icon: solIcon.src,
+		title: "Solana",
+		symbol: "SOL",
+		price: "142.89",
+		change: "0.5",
+		changeStat: "dec",
+	},
+	{
+		icon: adaIcon.src,
+		title: "Cardano",
+		symbol: "ADA",
+		price: "0.58",
+		change: "3.2",
+		changeStat: "acs",
+	},
+];
+
 export default function HeroSection() {
 	return (
-		<section className="h-78.5">
+		<section className="h-78.5 flex justify-between items-center">
 			<div className="w-181.75 h-full flex flex-col justify-start items-start gap-5">
 				<div className="font-bold text-[73px] tracking-[-3] leading-14">
 					Track Crypto <span className="text-(--green-color)">Prices</span> in
@@ -15,12 +49,48 @@ export default function HeroSection() {
 					<button className="bg-(--green-color) text-center rounded-md text-white cursor-pointer py-2 px-4 font-semibold">
 						Get Started
 					</button>
-					<button className="border border-black/10 shadow-sm text-center rounded-md text-black cursor-pointer py-2 px-4 font-semibold">
+					<button className="border border-black/5 shadow-sm text-center rounded-md text-black cursor-pointer py-2 px-4 font-semibold">
 						Learn More
 					</button>
 				</div>
 			</div>
-			<div></div>
+			<div>
+				<div className="w-[288px] h-61 border border-black/5 rounded-2xl shadow-sm shadow-black/5 relative flex flex-col justify-center items-center gap-2">
+					{currencyList.map((currency) => (
+						<div
+							key={currency.title}
+							className="w-60 h-11 flex justify-between items-center"
+						>
+							<div className="flex justify-start items-center gap-1">
+								<img src={currency.icon} />
+								<div className="flex flex-col justify-center items-start">
+									<span className="font-bold">{currency.title}</span>
+									<span className="text-(--description-text) text-sm">
+										{currency.symbol}
+									</span>
+								</div>
+							</div>
+							<div className="flex flex-col justify-center items-end">
+								<span className="font-bold">{`$${currency.price}`}</span>
+								<div className="flex items-center justify-center">
+									<img
+										src={
+											currency.changeStat == "acs" ? acsIcon.src : decIcon.src
+										}
+									/>
+									<span
+										className={`text-sm ${
+											currency.changeStat == "acs"
+												? "text-(--green-color)"
+												: "text-(--red-color)"
+										}`}
+									>{`${currency.change}%`}</span>
+								</div>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
 		</section>
 	);
 }
