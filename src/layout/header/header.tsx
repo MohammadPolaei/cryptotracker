5d561d91d52259fd9b410d9a2286cb88c2b904d6
Mohammad Polaei <mohammadpolaei@gmail.com>
2026-07-04 13:35:31 +0330

    feat : initial components and project structure created

---
+++ src/layout/header/header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+	return (
+		<div className="w-full flex justify-between items-center">
+			<div>LOGO</div>
+			<div>NAV</div>
+			<div>THEME BUTTON</div>
+		</div>
+	);
+}
