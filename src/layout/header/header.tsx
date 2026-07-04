c9fb7177f76636e951e439ddafcf0a806af4400a
Mohammad Polaei <mohammadpolaei@gmail.com>
2026-07-04 13:44:38 +0330

    feat : navbar items added

--- src/layout/header/header.tsx
+++ src/layout/header/header.tsx
@@ -1,8 +1,23 @@
+import Link from "next/link";
+
+const navBarList = [
+	{ title: "Tokens", href: "/" },
+	{ title: "Features", href: "/" },
+	{ title: "FAQ", href: "/" },
+	{ title: "Contact", href: "/" },
+];
+
 export default function Header() {
 	return (
 		<div className="w-full flex justify-between items-center">
 			<div>LOGO</div>
-			<div>NAV</div>
+			<nav className="flex justify-between items-center w-[273.68px] h-5">
+				{navBarList.map((navItem) => (
+					<Link href={navItem.href} key={navItem.title}>
+						{navItem.title}
+					</Link>
+				))}
+			</nav>
 			<div>THEME BUTTON</div>
 		</div>
 	);
